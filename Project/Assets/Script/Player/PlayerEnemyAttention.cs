950ea2bf3b153b3c598af8ac92df4c3f39ba700a
João Vitor dos Santos honorato <www.jotave@gmail.com>
2015-01-24 21:20:25 -0300

    Setando Player e Enimigos no novo Cenário

--- Project/Assets/Script/Player/PlayerEnemyAttention.cs
+++ Project/Assets/Script/Player/PlayerEnemyAttention.cs
@@ -13,7 +13,7 @@ public class PlayerEnemyAttention : MonoBehaviour {
         if (other.tag == "Enemy"){
             Enemy enemy = other.GetComponent<Enemy>();
 
-            print("enemy.range " + enemy.range + " : dist " +  Vector3.Distance(other.transform.position, transform.position));
+            //print("enemy.range " + enemy.range + " : dist " +  Vector3.Distance(other.transform.position, transform.position));
 
             if(Vector3.Distance(other.transform.position, transform.position) < enemy.range)
                 other.GetComponent<Enemy>().moveToContraPlayer = true;
