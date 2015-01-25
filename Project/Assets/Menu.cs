bdd5ba6948f771d9a01677a9c0c1b0011e358691
m1thr4nd1r <vitocr_santos@hotmail.com>
2015-01-25 04:35:10 -0300

    Adicionando primeira versão do Menu

---
+++ Project/Assets/Menu.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public static void Quit(){
+		print ("Saiu");
+		Application.Quit ();
+	}
+
+	public static void StartGame()
+	{
+
+	}
+}
